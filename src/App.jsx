798dea54cc539b038e438d829eea4d14039e9c08
unknown <hasiniperiyavaram@gmail>
2025-03-14 13:51:13 +0530

    added files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hasini periyavaram</h1> 
+      <p>Contact Details:</p>
+      <ul>
+        <li>Email: hasiniperiyavaram@gmail.com</li>
+      </ul>
+    </div>
+  );
+};
+
+export default App;
